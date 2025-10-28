68d6457df1d1587e3a20d6c8fd196489f97dc183
Raseduzzaman-Rayhan <raseduzzamanrayhan@gmail.com>
2025-10-28 09:58:51 +0600

    learned about input string and null character

---
+++ string_input.c
@@ -0,0 +1,34 @@
+// #include <stdio.h>
+// int main()
+// {
+//     char s[10];
+//     scanf("%s", &s); // single line string scan
+//     // printf("%c", s[2]); // single character print
+//     // printf("%c", s[1]); // single character print
+//     // printf("%c", s[7]); // single character print
+//     // printf("%d", s[7]); // single character ascii value print
+//     // printf("%d\n", s[5]); // null character value print
+//     printf("%s", s); // single line string print
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     char s[10];
+//     printf("%d\n", s[5]); // garbage value checking
+//     scanf("%s", &s);
+//     printf("%s\n", s);
+//     printf("%d", s[5]); // null character value checking
+//     return 0;
+// }
+
+#include <stdio.h>
+int main()
+{
+
+    char s[20];
+    scanf("%s", &s);
+    printf("%s", s);
+    return 0;
+}
